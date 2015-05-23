cfebb341761ca404efd29a09399375ba284a82ca
Rong <rongxiaosong@gmail.com>
2015-05-23 23:29:06 +0800

    fix

--- leveldb_client.py
+++ leveldb_client.py
@@ -22,23 +22,3 @@ class Client(object):
         msg = self.socket.recv_multipart()
         data = json.loads(msg[0])
         return data
-
-
-def test():
-    import optparse
-    optparser = optparse.OptionParser()
-    optparser.add_option('-i', '--host', dest='host', default='localhost')
-    optparser.add_option('-p', '--port', type='int', dest='port')
-    options, args = optparser.parse_args()
-    if not options.port:
-        optparser.print_help()
-        return
-
-    client = Client(options.host, options.port)
-    r = client.call('Get', {'key':'name'})
-    print r
-    client.close()
-
-
-if __name__ == '__main__':
-    test()
