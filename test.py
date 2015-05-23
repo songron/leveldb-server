b21936411d4945dfe090638b721e37d3af2f7416
Rong <rongxiaosong@gmail.com>
2015-05-24 00:02:56 +0800

    add test program

--- test.py
+++ test.py
@@ -7,28 +7,33 @@ from leveldb_client import Client
 
 
 host = '127.0.0.1'
-port = 5678
-dbpath = 'temp/db'
+port = 5999
+dbpath = 'temp.db'
 
 
-cmd = 'python leveldb_server.py -p %d -d %s &' % (port dbpath)
+cmd = 'python leveldb_server.py -p %d -d %s &' % (port, dbpath)
 os.system(cmd)
+print 'Start Server OK!'
 
 
 client = Client(host, port)
 
-for i in xrange(1000):
-    key = str(i)
-    val = str(i*1000)
+start = ord('a')
+for i in xrange(26):
+    key = chr(start+i)
+    val = str(i)
     client.call('Put', {'key': key, 'value': val})
 
-v = client.call('Get', {'key': 34})
+print 'Insert OK!'
+
+v = client.call('Get', {'key': 'b'})
 print v
 
 
-r = client.call('RangeIter', {'key_from': '100', 'key_to': '200'})
+r = client.call('RangeIter', {'key_from': 'a', 'key_to': 'f'})
 print len(r)
 print r
 
 
 client.close()
+print 'Finished!'
